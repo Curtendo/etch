d9a05477df648e533cfa2146cbc584fe93dd2195
Curtis CHROME <mrmr450@gmail.com>
2024-01-13 10:13:47 +0900

    Create 16x16 grid using JS.
    Using document fragment allows the grid to be built virtually
    before finally being appended to the main container, requiring only
    one DOM update.

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-let mainContainer = document.querySelector(".main-container");
+const mainContainer = document.querySelector(".main-container");
+const fragment = new DocumentFragment();
 
+const amount = 16;
+
+for (let i = 0; i < amount; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < amount; j++) {
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        row.appendChild(grid);
+    }
+
+    fragment.appendChild(row);
+}
+mainContainer.appendChild(fragment);
