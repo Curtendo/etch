fe03969440fa74e1d827182e684036de7e6ece0a
Curtis CHROME <mrmr450@gmail.com>
2024-01-13 15:08:01 +0900

    Implement dimension button and logic

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 const mainContainer = document.querySelector(".main-container");
 const fragment = new DocumentFragment();
 
-const amount = 16;
+let amount = 16;
 
-for (let i = 0; i < amount; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
+generateGrid(amount);
 
-    for (let j = 0; j < amount; j++) {
-        let grid = document.createElement("div");
-        grid.classList.add("grid");
-        row.appendChild(grid);
+function generateGrid(amount) {
+    for (let i = 0; i < amount; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < amount; j++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            row.appendChild(grid);
+        }
+
+        fragment.appendChild(row);
     }
-
-    fragment.appendChild(row);
+    mainContainer.appendChild(fragment);
 }
-mainContainer.appendChild(fragment);
+
+mainContainer.addEventListener("mouseover", function(e) {
+    if (e.target.className === "grid") {
+        e.target.classList.add("grid-dark");
+    }
+})
+
+const promptUser = document.querySelector("button");
+promptUser.addEventListener("click", function(e) {
+    do {
+        amount = prompt("Please enter a number (<100) for grid dimensions.", 16);
+    } while (amount > 100 && amount < 1);
+    
+    mainContainer.replaceChildren();
+    generateGrid(amount);
+})
